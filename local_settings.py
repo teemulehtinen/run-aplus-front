d143bc95581c5a3f00c4811a9dbbcb9387b2928a
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-08-21 20:35:41 +0300

    Disable cache by default

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,17 @@
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '/db/db.sqlite3',
     }
 }
+
+if not os.environ.get('APLUS_TEST_CACHE'):
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
 
 OVERRIDE_SUBMISSION_HOST = 'http://plus:8000'
