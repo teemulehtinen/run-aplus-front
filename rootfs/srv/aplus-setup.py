faf24887714e8622cbf25731375f47e741e407a0
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2018-10-29 14:42:21 +0200

    Add assistant user

--- rootfs/srv/aplus-setup.py
+++ rootfs/srv/aplus-setup.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 def create_default_users():
     from django.contrib.auth.models import User
 
-    u1 = User.objects.create(
+    ur = User.objects.create(
         username="root",
         email="root@localhost",
         first_name="Ruth",
@@ -16,21 +16,34 @@ def create_default_users():
         is_superuser=True,
         is_staff=True,
     )
-    u1.set_password("root")
-    u1.save()
+    ur.set_password("root")
+    ur.save()
 
-    u2 = User.objects.create(
+    ua = User.objects.create(
+        username="assistant",
+        email="assistant@localhost.invalid",
+        first_name="Aaron",
+        last_name="Assistant",
+    )
+    ua.set_password("assistant")
+    ua.save()
+    ua.userprofile.student_id = "133701"
+    ua.userprofile.save()
+
+    us = User.objects.create(
         username="student",
         email="student@localhost",
         first_name="Stacey",
-        last_name="Smith",
+        last_name="Student",
     )
-    u2.set_password("student")
-    u2.save()
-    u2.userprofile.student_id = "123456"
-    u2.userprofile.save()
+    us.set_password("student")
+    us.save()
+    us.userprofile.student_id = "123456"
+    us.userprofile.save()
 
-def create_default_courses():
+    return {'root': ur.userprofile, 'teacher': ur.userprofile, 'assistant': ua.userprofile, 'student': us.userprofile}
+
+def create_default_courses(users):
     from course.models import Course, CourseInstance
 
     course = Course.objects.create(
@@ -38,6 +51,8 @@ def create_default_courses():
         code="DEF000",
         url="def",
     )
+    course.teachers.set([users['teacher']])
+
     today = timezone.now()
     instance = CourseInstance.objects.create(
         course=course,
@@ -47,11 +62,16 @@ def create_default_courses():
         ending_time=today + timedelta(days=365),
         configure_url="http://grader:8080/default/aplus-json",
     )
+    instance.assistants.set([users['assistant']])
+
+    return {'default': instance}
 
 def create_default_services():
     from external_services.models import LTIService
 
-    service = LTIService.objects.create(
+    services = {}
+
+    services['rubyric+'] = LTIService.objects.create(
         url="http://localhost:8090/",
         menu_label="Rubyric+",
         menu_icon_class="save-file",
@@ -59,12 +79,22 @@ def create_default_services():
         consumer_secret="bar",
     )
 
+    services['rubyric'] = LTIService.objects.create(
+        url="http://localhost:8091/",
+        menu_label="Rubyric",
+        menu_icon_class="save-file",
+        consumer_key="foo",
+        consumer_secret="bar",
+    )
+
+    return services
+
 
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aplus.settings")
     sys.path.insert(0, '')
     django.setup()
 
-    create_default_users()
-    create_default_courses()
-    create_default_services()
+    users = create_default_users()
+    courses = create_default_courses(users)
+    services = create_default_services()
