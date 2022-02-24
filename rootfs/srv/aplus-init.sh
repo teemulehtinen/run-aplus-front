46ceeddd48aa647aaa81a97a798832d279f32399
Markku Riekkinen <markku.riekkinen@aalto.fi>
2022-02-24 14:48:06 +0200

    Simplify the way the configure URL is set to the gitmanager
    Using an execline script was unnecessary for this case.
    The execline idea was copied from the services.d run scripts,
    but the aplus-init.sh script is executed in a context that already
    includes the setup for the user home directory and working directory.

--- rootfs/srv/aplus-init.sh
+++ rootfs/srv/aplus-init.sh
@@ -4,7 +4,7 @@
 #start_services
 if [ $USE_GITMANAGER = 'true' ]; then
     run_services aplus-lti-services
-    /srv/aplus-set-configure-url def/current 'http://gitmanager:8070/default/aplus-json'
+    setuidgid $USER python3 manage.py reload_course_configuration --no-reload --url 'http://gitmanager:8070/default/aplus-json' def/current
 else
     # start course updater (will exit when successful)
     run_services aplus-course-update aplus-lti-services
